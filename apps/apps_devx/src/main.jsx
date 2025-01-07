da450df1fd7aeb7b568c63e4501c161eac444c78
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2025-01-07 21:58:19 +0100

    remove strictMode

--- apps/apps_devx/src/main.jsx
+++ apps/apps_devx/src/main.jsx
@@ -7,12 +7,12 @@ import TodoApp3 from "./apps/TodoApp3";
 import GalleryApp from "./apps/GalleryApp";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <>
     {/* Todo Basic */}
     {/* <TodoApp /> */}
     {/* Todo useEffect and LocalStorage */}
     {/* <TodoApp3 /> */}
     {/* Todo useLocalStorage */}
     <GalleryApp />
-  </StrictMode>
+  </>
 );
